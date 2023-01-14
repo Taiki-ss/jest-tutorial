193cfbb51afeb5215e64392694f8b8bc1aa51a17
Taiki-nk <taikifate.0206@gmail.com>
2023-01-14 11:42:18 +0900

    testケース追加

--- isZero.test.ts
+++ isZero.test.ts
@@ -4,3 +4,10 @@ test("0を渡したらtrueになること", () => {
   const result = isZero(0);
   expect(result).toBe(true);
 });
+
+test("0以外を渡したらfalseになること", () => {
+  for (let i = 1; i <= 10; i++) {
+    const result = isZero(i);
+    expect(result).toBe(false);
+  }
+});
